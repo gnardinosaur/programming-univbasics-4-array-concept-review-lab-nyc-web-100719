b2355c8b9dc09e8eaf65fba2eb460e0b3a40d7fd
passionate-optimization-7504 <nicholasjardine9@gmail.com>
2019-09-20 17:57:53 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,7 +4,7 @@ def find_element_index(array, value_to_find)
   array.length.times do |index|
     #compare values 
     if array[index] == value_to_find
-      #return index 
+      #if true reassign value_index to that index
      value_index = index
     end 
   end 
